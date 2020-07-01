d36ac87a8b71d431f421fb82835c029c15b0db58
Augusto Silva <gutim160@gmail.com>
2020-06-30 21:44:30 -0300

    composicao-exercicios resolvido PARTE 01

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,24 @@
 package aplication;
 
+import java.util.Date;
+
+import entites.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Boa Noite!");
-
+		Order order = new Order(1080, new Date(), OrderStatus.PEDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 
 }
