f0a7ec800aefce932d8f75148e0b837cf20d85bc
Augusto Silva <gutim160@gmail.com>
2020-07-20 08:55:01 -0300

    Implementação da interface

--- src/application/comparetor/entities/Product.java
+++ src/application/comparetor/entities/Product.java
@@ -36,6 +36,6 @@ public class Product {
 	
 	@Override
 	public String toString() {
-		return "Product [name=" + name + ", price=" + price + "]";
+		return this.name + ", " + String.format("%.2f", price);
 	}
 }
